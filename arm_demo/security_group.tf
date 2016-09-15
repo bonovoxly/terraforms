2b9a577f3e26fe2549a813ac5e61653bbb929966
Bill Cawthra <bcawthra@gmail.com>
2016-09-15 11:16:42 +0000

    added more security groups to the demo

--- arm_demo/security_group.tf
+++ arm_demo/security_group.tf
@@ -1,19 +1,62 @@
-# bastionhost security group
-resource "azurerm_network_security_group" "sg_bastionhost" {
-  name                  = "sg_bastionhost"
-  location              = "East US"
-  resource_group_name   = "${azurerm_resource_group.resource_group.name}"
+# set security group
+resource "azurerm_network_security_group" "sg-bastionhost" {
+    name = "sg-bastionhost"
+    location = "East US"
+    resource_group_name   = "${azurerm_resource_group.resource_group.name}"
 
-  security_rule {
-    name                        = "allow_ssh"
-    priority                    = 1000
-    direction                   = "Inbound"
-    access                      = "Allow"
-    protocol                    = "Tcp"
-    source_port_range           = "*"
-    destination_port_range      = "22"
-    source_address_prefix       = "*"
-    destination_address_prefix  = "*"
-  }
+    security_rule {
+        name = "default-allow-ssh"
+        priority = 1000
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "22"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
 
+    security_rule {
+        name = "deny-all"
+        priority = 10001
+        direction = "Inbound"
+        access = "Deny"
+        protocol = "*"
+        source_port_range = "*"
+        destination_port_range = "*"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+
+}
+
+# set security group
+resource "azurerm_network_security_group" "sg-internal" {
+    name = "sg-internal"
+    location = "East US"
+    resource_group_name   = "${azurerm_resource_group.resource_group.name}"
+
+    security_rule {
+        name = "default-allow-ssh"
+        priority = 1000
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "22"
+        source_address_prefix = "10.240.0.4/32"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name = "deny-all"
+        priority = 10001
+        direction = "Inbound"
+        access = "Deny"
+        protocol = "*"
+        source_port_range = "*"
+        destination_port_range = "*"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
 }
