d7b274f717a8577cd9d525e6bddefd4c5e525609
Bill Cawthra <bcawthra@gmail.com>
2016-09-15 11:45:09 +0000

    fixed deny security rule priority

--- ARM/demo/security_group.tf
+++ ARM/demo/security_group.tf
@@ -18,7 +18,7 @@ resource "azurerm_network_security_group" "sg-bastionhost" {
 
     security_rule {
         name = "deny-all"
-        priority = 10001
+        priority = 6001
         direction = "Inbound"
         access = "Deny"
         protocol = "*"
