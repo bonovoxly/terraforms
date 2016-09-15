c1dce92b8b5d150a49033a4bc9352024210ec155
Bill Cawthra <bcawthra@gmail.com>
2016-09-15 11:41:42 +0000

    added storage account

---
+++ ARM/demo/storageaccount.tf
@@ -0,0 +1,8 @@
+# configure storage account
+resource "azurerm_storage_account" "storageaccount" {
+    name = "${var.environment}storageaccount"
+    resource_group_name = "${azurerm_resource_group.resource_group.name}"
+    location = "eastus"
+    account_type = "Standard_LRS"
+
+}
