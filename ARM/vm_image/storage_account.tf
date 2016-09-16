972e60dd4d3f4db38f8ee0c4e00ee7d579721ce2
Bill Cawthra <bcawthra@gmail.com>
2016-09-16 11:04:26 +0000

    updated storage account resource

--- ARM/vm_image/storage_account.tf
+++ ARM/vm_image/storage_account.tf
@@ -1,5 +1,5 @@
 # configure storage account
-resource "azurerm_storage_account" "storageaccount" {
+resource "azurerm_storage_account" "vmimage-storageaccount" {
     name = "${var.environment}storageaccount"
     resource_group_name = "${azurerm_resource_group.resource_group.name}"
     location = "eastus"
