6443e3d1a97d1f080afb28e69529b84a22d7107f
Bill Cawthra <bill@siliconharborlabs.com>
2017-08-20 11:00:14 -0400

    simplified aws_credentials source script

--- aws_credentials.sh
+++ aws_credentials.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
-# configuring credentials for Terraform
-# only useful if you save your credentials in `~/.aws/config`
+CREDENTIAL_FILE=~/.aws/config
 
-# If no variable is defined, use the default `~/.aws/config`.
-if [ $# -eq 0 ]; then
-  CREDENTIAL_FILE=~/.aws/config
-else
-  CREDENTIAL_FILE=$1
-fi
+export AWS_ACCESS_KEY_ID=`cat $CREDENTIAL_FILE | grep -v "^#.*" | grep aws_access_key_id | awk '{ print $NF }'`
+export AWS_SECRET_ACCESS_KEY=`cat $CREDENTIAL_FILE | grep -v "^#.*" | grep aws_secret_access_key | awk '{ print $NF }'`
 
-# Check if the file exists.
-if [ ! -f $CREDENTIAL_FILE ] ; then
-  echo "Credentials file $CREDENTIAL_FILE does not exist.  Be sure the AWS credentials file is specified (ie, ~/.aws/config).  Exiting."
-  exit 1
-fi
-
-export AWS_ACCESS_KEY_ID=`grep aws_access_key_id $CREDENTIAL_FILE | awk '{ print $NF }'`
-export AWS_SECRET_ACCESS_KEY=`grep aws_secret_access_key $CREDENTIAL_FILE | awk '{ print $NF }'`
